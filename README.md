a817a0dcbd464fa997e0129aee520e853824e26c
Sanjog Jadhav <sanjogj43@gmail.com>
2019-12-05 00:11:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PEPGuidelines
+ PEP Guidelines
---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+# from MyModule import foo, bar, foobar
+
+
+def my_function(one, two,
+                three, four,
+                five, six):
+    print("Hello World")
+
+
+def sec_fuct():
+    print("Sec func")
+
+
+lst = [1, 2,
+       3, 4,
+       5, 6,
+       7]
+
+check = False
+if check is True:
+    print("This is True")
